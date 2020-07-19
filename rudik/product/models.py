7d2f689f506d8f454d9991d4bdbcb0b76e71e6be
vruslan62 <ruslanval62@gmail.com>
2020-07-19 23:54:03 +0300

    added Category and Product models

--- rudik/product/models.py
+++ rudik/product/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+
+class Category(TimeStampedModel, models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(TimeStampedModel, models.Model):
+    name = models.CharField(max_length=256)
+    category = models.ForeignKey(
+        "product.Category", on_delete=models.PROTECT, related_name="products"
+    )
+
+    def __str__(self):
+        return self.name
