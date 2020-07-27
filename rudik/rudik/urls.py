c7716835f4b81fa269094e9981c1f7abab1a5568
vruslan62 <ruslanval62@gmail.com>
2020-07-28 01:38:33 +0300

    added media path

--- rudik/rudik/urls.py
+++ rudik/rudik/urls.py
@@ -1,5 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from .admin import rudik_site
 
-urlpatterns = [path("admin/", rudik_site.urls)]
+urlpatterns = [path("admin/", rudik_site.urls)] + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
