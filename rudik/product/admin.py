743f05cb01bfaf45e2074a74cb5b8b25ce21f9c4
vruslan62 <ruslanval62@gmail.com>
2020-07-28 09:26:36 +0300

    renamed class CategoryImageAdmin to CategoryImageTabularInline

--- rudik/product/admin.py
+++ rudik/product/admin.py
@@ -9,21 +9,21 @@ from .models import Product
 from .models import ProductImage
 
 
-class ProductImageAdmin(admin.TabularInline):
-    model = ProductImage
+class CategoryImageTabularInline(admin.TabularInline):
+    model = CategoryImage
     extra = 1
 
 
-class CategoryImageAdmin(admin.TabularInline):
-    model = CategoryImage
+class ProductImageTabularInline(admin.TabularInline):
+    model = ProductImage
     extra = 1
 
 
 @admin.register(Category, site=rudik_site)
 class CategoryAdmin(MPTTModelAdmin):
-    inlines = [CategoryImageAdmin]
+    inlines = [CategoryImageTabularInline]
 
 
 @admin.register(Product, site=rudik_site)
 class ProductAdmin(admin.ModelAdmin):
-    inlines = [ProductImageAdmin]
+    inlines = [ProductImageTabularInline]
