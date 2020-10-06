5a09ce99800810f37f63f990f1b48992be6c9988
vruslan62 <ruslanval62@gmail.com>
2020-10-07 00:03:15 +0300

    extend Product serializer

--- rudik/product/endpoints/product.py
+++ rudik/product/endpoints/product.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from rest_framework import viewsets
 
+from core.paginations import SmallPageNumberPagination
+
+from ..models import Configuration
+from ..models import ConfigurationType
 from ..models import Product
 from ..models import ProductImage
+from ..models import ProductVariant
 
 
 class ProductImageSerializer(serializers.ModelSerializer):
@@ -11,15 +16,40 @@ class ProductImageSerializer(serializers.ModelSerializer):
         fields = ["image", "is_default"]
 
 
+class ConfigurationTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ConfigurationType
+        fields = ["id", "name", "is_color"]
+
+
+class ConfigurationSerializer(serializers.ModelSerializer):
+    type = ConfigurationTypeSerializer()
+
+    class Meta:
+        model = Configuration
+        fields = ["id", "type", "name", "value", "type"]
+
+
+class ProductVariantSerializer(serializers.ModelSerializer):
+    configurations = ConfigurationSerializer(many=True)
+    qty = serializers.IntegerField(source="count_rest")
+
+    class Meta:
+        model = ProductVariant
+        fields = ["id", "qty", "price", "configurations"]
+
+
 class ProductSerializer(serializers.ModelSerializer):
     images = ProductImageSerializer(many=True)
+    variants = ProductVariantSerializer(many=True)
 
     class Meta:
         model = Product
-        fields = ["id", "name", "category", "images"]
+        fields = ["id", "name", "category", "images", "variants"]
 
 
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Product.objects.all()
+    queryset = Product.objects.order_by("id")
     serializer_class = ProductSerializer
+    pagination_class = SmallPageNumberPagination
     filterset_fields = ["category"]
